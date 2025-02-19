65f1317ae22c1e5efcff0cefb03000d474213c9c
Ruan Richard <Richard_10luan@hotmail.com>
2025-02-19 00:55:04 -0300

    up json

---
+++ assets/scripts/options.js
@@ -0,0 +1,8 @@
+let burgerJson = [
+    {id:1, name:'Texas Burger', img:'assets/img/burges/burger1.png', price:25.58, description:'Descrição da em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Double Cheddar', img:'assets/img/burges/burger2.png', price:25.58, description:'Descrição da em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Salid Burger', img:'assets/img/burges/burger3.png', price:26.89, description:'Descrição da em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Monster Burger', img:'assets/img/burges/burger4.png', price:28.98, description:'Descrição da em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Batata', img:'assets/img/burges/batata.png', price:14.55, description:'Descrição da em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Combo1', img:'assets/img/burges/combo1.png', price:38.36, description:'Descrição da em mais de uma linha muito legal bem interessante'}
+];
